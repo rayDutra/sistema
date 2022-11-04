a06bfdae759261ee85e1493d0e5e47da333a5d28
iure11 <iurematheus11@gmail.com>
2022-11-04 20:18:04 -0300

    Primeiro Commit

---
+++ src/test/java/apartamento/sistema/SistemaApplicationTests.java
@@ -0,0 +1,13 @@
+package apartamento.sistema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
